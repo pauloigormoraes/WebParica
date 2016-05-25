d09a4ac7e817846594cda6b841455f8b3104071b
Allex Lima <allexlima97@gmail.com>
2016-05-25 19:26:35 -0400

    link das paginas

--- view/master.php
+++ view/master.php
@@ -40,10 +40,9 @@
 
     <ul class="nav menu">
         <li><a href="#home"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg> Início</a></li>
-        <li><a href="#link"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg> Início</a></li>
-        <li><a href="#link"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Buscar aluno</a></li>
-        <li><a href="#link"><svg class="glyph stroked table"><use xlink:href="#stroked-clipboard-with-paper"></use></svg> Declarações/Atas</a></li>
-        <li><a href="#link"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Matricular</a></li>
+        <li><a href="#buscar"><svg class="glyph stroked table"><use xlink:href="#stroked-table"></use></svg> Buscar aluno</a></li>
+        <li><a href="#declaracao"><svg class="glyph stroked table"><use xlink:href="#stroked-clipboard-with-paper"></use></svg> Declarações/Atas</a></li>
+        <li><a href="#formulario"><svg class="glyph stroked pencil"><use xlink:href="#stroked-pencil"></use></svg> Matricular</a></li>
         <li role="presentation" class="divider"></li>
     </ul>
 
