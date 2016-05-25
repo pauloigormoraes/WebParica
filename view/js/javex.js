4b9b48bee9f04ad7e6cb48ee7a0b76ebf6aa4331
Allex Lima <allexlima97@gmail.com>
2016-05-25 19:20:52 -0400

    Integracao ajax parte 1, master

--- view/js/javex.js
+++ view/js/javex.js
@@ -1,3 +1,21 @@
-/**
- * Created by allex on 5/25/16.
- */
+$(document).ready(function(){
+    function showPage(page, when, action){
+        if(when){
+            params = when.split(":");
+            $(params[1]).on(params[0], function(){
+                $("#MainFrame").load( "view/" + page + ".php", action);
+            });
+        }else
+            $("#MainFrame").load( "view/" + page + ".php", action);
+    }
+
+    $('a').bind('click',function(event) {
+        var anchor = $(this).attr('href');
+        anchor = anchor.split("#")
+        anchor = anchor[1]
+
+        showPage(anchor)
+    });
+
+
+});
--- view/master.php
+++ view/master.php
@@ -49,8 +49,7 @@
 
 </div>
 
-<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
-
+<div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main" id="MainFrame">
 
 </div>
 
@@ -61,9 +60,9 @@
 <script src="view/js/easypiechart.js"></script>
 <script src="view/js/easypiechart-data.js"></script>
 <script src="view/js/bootstrap-datepicker.js"></script>
+<script src="view/js/javex.js"></script>
+
 <script>
-    $('#calendar').datepicker({
-    });
 
     !function ($) {
         $(document).on("click","ul.nav li.parent > a > span.icon", function(){
