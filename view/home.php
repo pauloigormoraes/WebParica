591ad67af21a1ad82aacd94ac2d3416327b24f19
paulomoraes <higoormoraes@gmail.com>
2016-05-26 12:37:57 -0400

    Alteracoes semifinais nas views

--- view/home.php
+++ view/home.php
@@ -8,12 +8,14 @@
 <div class="row" style="text-align: center;">
 	<div class="col-lg-12" style="margin-top: 120px">
 		<img src='view/img/leafa.png'>
-		<h1 class="page-header" style="color: #91DC5A !important;">Bem vindo(a) ao Sistema Acadêmico - PARICÁ</h1>
+		<h1 class="page-header font" style="color: #91DC5A !important; text-shadow: 10px 10px 10px #99FF99;">
+			<b>Bem vindo(a) ao Sistema Acadêmico - PARICÁ</b></h1>
 	</div>
 </div>
-<!---->
-<!--<div class="row">-->
-<!--	<div class="index">-->
-<!--		<img src='view/img/castle.png'>-->
-<!--	</div>-->
-<!--</div>-->
+
+<div>
+	<div class="col-lg-12" style="margin-top: 140px">
+		<h1 class="font" style="font-size: 13px !important; text-align: center">
+			<p><a style="color: #777 !important;" href="http://www.moraespaulo.com/" target="_blank">Paulo Moraes,</a> <a style="color: #777 !important;" href="http://www.allexlima.com/" target="_blank">Allex Lima</a> e <a style="color: #777 !important;" href="http://www.anticitera.com.br/" target="_blank">Daniel Bispo</a> - UniNorte Laureate &copy; Todos os direitos reservados</p></h1>
+	</div>
+</div>
