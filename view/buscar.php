3697302f27639b72ac187967a82b31314a3c9a61
paulomoraes <higoormoraes@gmail.com>
2016-05-26 02:21:25 -0400

    Ajuste em buscar.php

--- view/buscar.php
+++ view/buscar.php
@@ -21,85 +21,86 @@
 
 						<div class="form-group col-md-12">
 							<label>Nome do aluno</label>
-							<input class="form-control" type="text">
+							<input name="nome_aluno" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>CPF</label>
-							<input class="form-control" type="text">
+							<input name="cpf_aluno" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Data de Nascimento</label>
-							<input class="form-control" type="date">
+							<input name="dt_nasc" class="form-control" type="date">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Estado Civil</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>Solteiro</option>
-								<option>Casado</option>
-								<option>Divorciado</option>
-								<option>Viúvo</option>
+							<select name="estadoCivil" class="form-control" required>
+								<option value="" disabled selected>Selecione</option>
+								<option value="0">Solteiro</option>
+								<option value="1">Casado</option>
+								<option value="2">Divorciado</option>
+								<option value="3">Viúvo</option>
+								<option value="4">Estável</option>
 							</select>
 						</div>
 
 						<div class="form-group col-md-8">
 							<label>Certidão de Nascimento</label>
-							<input class="form-control" type="text">
+							<input name="cert_nasc" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Matrícula do Aluno</label>
-							<input class="form-control" type="text">
+							<input name="matri_aluno" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>PcD</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>Sim</option>
-								<option>Não</option>
+							<select name="pcd" class="form-control" required>
+								<option value="" disabled selected>Selecione</option>
+								<option value="0">Sim</option>
+								<option value="1">Não</option>
 							</select>
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Bairro</label>
-							<input class="form-control" type="text">
+							<input name="bairro" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Estado</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>Acre</option>
-								<option>Alagoas</option>
-								<option>Amapá</option>
-								<option>Amazonas<master/option>
-								<option>Bahia</option>
-								<option>Ceará</option>
-								<option>Distrito Federal</option>
-								<option>Espírito Santo</option>
-								<option>Goiás</option>
-								<option>Maranhão</option>
-								<option>Mato Grosso</option>
-								<option>Mato Grosso do Sul</option>
-								<option>Minas Gerais</option>
-								<option>Pará</option>
-								<option>Paraíba</option>
-								<option>Paraná</option>
-								<option>Pernambuco</option>
-								<option>Piauí</option>
-								<option>Rio de Janeiro</option>
-								<option>Rio Grande do Norte</option>
-								<option>Rio Grande do Sul</option>
-								<option>Rondônia</option>
-								<option>Roraima</option>
-								<option>Santa Catarina</option>
-								<option>São Paulo</option>
-								<option>Sergipe</option>
-								<option>Tocantins</option>
+							<select name="estado" class="form-control" required>
+								<option value="" disabled selected>Selecione</option>
+								<option value="AC">Acre</option>
+								<option value="AL">Alagoas</option>
+								<option value="AP">Amapá</option>
+								<option value="AM">Amazonas</option>
+								<option value="BA">Bahia</option>
+								<option value="CE">Ceará</option>
+								<option value="DF">Distrito Federal</option>
+								<option value="ES">Espírito Santo</option>
+								<option value="GO">Goiás</option>
+								<option value="MA">Maranhão</option>
+								<option value="MT">Mato Grosso</option>
+								<option value="MS">Mato Grosso do Sul</option>
+								<option value="MG">Minas Gerais</option>
+								<option value="PA">Pará</option>
+								<option value="PB">Paraíba</option>
+								<option value="PR">Paraná</option>
+								<option value="PE">Pernambuco</option>
+								<option value="PI">Piauí</option>
+								<option value="RJ">Rio de Janeiro</option>
+								<option value="RN">Rio Grande do Norte</option>
+								<option value="RS">Rio Grande do Sul</option>
+								<option value="RO">Rondônia</option>
+								<option value="RR">Roraima</option>
+								<option value="SC">Santa Catarina</option>
+								<option value="SP">São Paulo</option>
+								<option value="SE">Sergipe</option>
+								<option value="TO">Tocantins</option>
 							</select>
 						</div>
 
@@ -117,50 +118,50 @@
 
 						<div class="form-group col-md-6">
 							<label>Turma</label>
-							<input class="form-control" type="text">
+							<input name="turma" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-6">
 							<label>Turno</label>
-							<input class="form-control" type="text">
+							<input name="turno" class="form-control" type="text">
 						</div>
 
 						<div class="form-group col-md-6">
 							<label>Ano/Serie</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>1º Ano</option>
-								<option>2º Ano</option>
-								<option>3º Ano</option>
-								<option>4º Ano</option>
-								<option>5º Ano</option>
-								<option>6º Ano</option>
-								<option>7º Ano</option>
-								<option>8º Ano</option>
-								<option>9º Ano</option>
-								<option>1º Ano/Médio</option>
-								<option>2º Ano/Médio</option>
-								<option>3º Ano/Médio</option>
+							<select name="ano_serie" class="form-control">
+								<option value="" disabled selected>Selecione</option>
+								<option value="0">1º Ano</option>
+								<option value="1">2º Ano</option>
+								<option value="2">3º Ano</option>
+								<option value="3">4º Ano</option>
+								<option value="4">5º Ano</option>
+								<option value="5">6º Ano</option>
+								<option value="6">7º Ano</option>
+								<option value="7">8º Ano</option>
+								<option value="8">9º Ano</option>
+								<option value="9">1º Ano/Médio</option>
+								<option value="10">2º Ano/Médio</option>
+								<option value="11">3º Ano/Médio</option>
 							</select>
 						</div>
 
 						<div class="form-group col-md-6">
 							<label>Situação</label>
-							<div class="radio">
+							<div name="situacao" class="radio">
 								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>  Novo
+									<input value="0" type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>  Novo
 								</label>
 
 								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">  Transferido
+									<input value="1" type="radio" name="optionsRadios" id="optionsRadios2" value="option2">  Transferido
 								</label>
 
 								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Desistente
+									<input value="2" type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Desistente
 								</label>
 
 								<label>
-									<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Repetente
+									<input value="3" type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Repetente
 								</label>
 							</div>
 						</div>
