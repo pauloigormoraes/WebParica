89b9e4b9ff44a6f86defd792f13b3a030df1dc24
Allex Lima <allexlima97@gmail.com>
2016-05-26 01:29:31 -0400

    Adaptacao p/ model do formulario de cadastro de alunos

--- view/formulario.php
+++ view/formulario.php
@@ -11,43 +11,41 @@
 	</div>
 </div>
 
-
 <div class="row">
-	<div class="col-lg-12">
-		<div class="panel panel-default">
-			<div class="panel-heading">Dados do Aluno</div>
-			<div class="panel-body">
-				<div class="col-md-12">
-					<form role="form">
-
+	<form id="inserirAlunos" onsubmit="return false">
+		<div class="col-lg-12">
+			<div class="panel panel-default">
+				<div class="panel-heading">Dados do Aluno</div>
+				<div class="panel-body">
+					<div class="col-md-12">
 						<div class="form-group col-md-12">
 							<label>Nome do aluno</label>
-							<input name="nome_aluno" class="form-control" type="text">
+							<input name="nome_aluno" class="form-control" type="text" placeholder="Nome completo sem abreviações" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>CPF</label>
-							<input name="cpf" class="form-control" type="text">
+							<input name="cpf_aluno" class="form-control" type="number" placeholder="Somente números" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>RG</label>
-							<input name="rg" class="form-control" type="text">
+							<input name="rg_aluno" class="form-control" type="number" placeholder="Somente números" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>Orgão Expedidor</label>
-							<input name="orgao_exp" class="form-control" type="text">
+							<input name="orgExp" class="form-control" type="text" placeholder="Somente a sigla" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>Data de Nascimento</label>
-							<input name="data_nasc" class="form-control" type="date">
+							<input name="dt_nasc" class="form-control" type="date" required>
 						</div>
 
 						<div class="form-group col-md-4">
 							<label>Estado Civil</label>
-							<select name="estado_civ" class="form-control">
+							<select name="estadoCivil" class="form-control" required>
 								<option value="" disabled selected>Selecione</option>
 								<option value="0">Solteiro</option>
 								<option value="1">Casado</option>
@@ -57,48 +55,95 @@
 							</select>
 						</div>
 
-						<div class="form-group col-md-8">
+						<div class="form-group col-md-6">
 							<label>Certidão de Nascimento</label>
-							<input name="certidao_nasc" class="form-control" type="text">
+							<input name="cert_nasc" class="form-control" type="number" placeholder="Somente números" required>
 						</div>
 
-						<div class="form-group col-md-4">
-							<label>E-mail</label>
-							<input name="email" class="form-control" type="email">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Telefone <i style="font-size: 13px">(incluir DDD)</i></label>
-							<input name="telefone" class="form-control" type="tel">
-						</div>
-
-						<div class="form-group col-md-4">
+						<div class="form-group col-md-2">
 							<label>PcD</label>
-							<select name="pcd" class="form-control">
+							<select name="pcd" class="form-control" required>
 								<option value="" disabled selected>Selecione</option>
 								<option value="0">Sim</option>
 								<option value="1">Não</option>
 							</select>
 						</div>
 
+					</div>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Dados da Mãe</div>
+				<div class="panel-body">
+					<div class="col-md-12">
 						<div class="form-group col-md-4">
-							<label>Endereço</label>
-							<input name="endereco" class="form-control" type="text">
+							<label>Nome</label>
+							<input name="nome_mae" class="form-control" type="text" required>
+						</div>
+
+						<div class="form-group col-md-4">
+							<label>CPF</label>
+							<input name="cpf_mae" class="form-control" type="number" placeholder="Somente números" required>
+						</div>
+
+						<div class="form-group col-md-4">
+							<label>RG</label>
+							<input name="rg_mae" class="form-control" type="number" placeholder="Somente números" required>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Dados do Pai</div>
+				<div class="panel-body">
+					<div class="col-md-12">
+						<div class="form-group col-md-4">
+							<label>Nome</label>
+							<input name="nome_pai" class="form-control" type="text" required>
+						</div>
+
+						<div class="form-group col-md-4">
+							<label>CPF</label>
+							<input name="cpf_pai" class="form-control" type="number" placeholder="Somente números" required>
+						</div>
+
+						<div class="form-group col-md-4">
+							<label>RG</label>
+							<input name="rg_pai" class="form-control" type="number" placeholder="Somente números" required>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="panel panel-default">
+				<div class="panel-heading">Dados para contato</div>
+				<div class="panel-body">
+					<div class="col-md-12">
+						<div class="form-group col-md-7">
+							<label>Rua</label>
+							<input name="lagradouro" class="form-control" type="text" required>
 						</div>
 
 						<div class="form-group col-md-2">
-							<label>Nº</label>
-							<input name="num_casa" class="form-control" type="number">
+							<label>Número</label>
+							<input name="numero" class="form-control" type="number" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>Bairro</label>
-							<input name="bairro" class="form-control" type="text">
+							<input name="rg_pai" class="form-control" type="text" required>
+						</div>
+
+						<div class="form-group col-md-3">
+							<label>Cidade</label>
+							<input name="cidade" class="form-control" type="text" required>
 						</div>
 
 						<div class="form-group col-md-3">
 							<label>Estado</label>
-							<select name="estado" class="form-control">
+							<select name="estado" class="form-control" required>
 								<option value="" disabled selected>Selecione</option>
 								<option value="AC">Acre</option>
 								<option value="AL">Alagoas</option>
@@ -130,102 +175,28 @@
 							</select>
 						</div>
 
-						<div class="form-group col-md-12">
-							<label>Observação</label>
-							<textarea name="obeservacao" class="form-control" rows="5"></textarea>
+						<div class="form-group col-md-3">
+							<label>Telefone 1</label>
+							<input name="tel_1" class="form-control" type="tel" required>
 						</div>
-					</form>
+
+						<div class="form-group col-md-3">
+							<label>Telefone 2</label>
+							<input name="tel_2" class="form-control" type="tel">
+						</div>
+
+						<div class="form-group col-md-8">
+							<label>E-mail</label>
+							<input name="email" class="form-control" type="email">
+						</div>
+					</div>
 				</div>
-
 			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-heading">Dados do Responsável</div>
-			<div class="panel-body">
-				<div class="col-md-12">
-					<form role="form">
 
-						<div class="form-group col-md-4">
-							<label>Nome</label>
-							<input name="nome_mae" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>CPF</label>
-							<input name="cpf_mae" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Gradu de Parentesco</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>Mãe</option>
-								<option>Pai</option>
-								<option>Avó/Avô</option>
-								<option>Tia/Tio</option>
-							</select>
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Nome</label>
-							<input name="nome_pai" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>CPF</label>
-							<input name="cpf_pai" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Grau de Parentesco</label>
-							<select class="form-control">
-								<option>Selecione</option>
-								<option>Mãe</option>
-								<option>Pai</option>
-								<option>Avó/Avô</option>
-								<option>Tia/Tio</option>
-							</select>
-						</div>
-					</form>
-				</div>
-
-			</div>
-		</div>
-		<div class="panel panel-default">
-			<div class="panel-heading">Dados Acadêmicos</div>
-			<div class="panel-body">
-				<div class="col-md-12">
-					<form role="form">
-
-						<div class="form-group col-md-4">
-							<label>Turma</label>
-							<input name="turma" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Turno</label>
-							<input name="turno" class="form-control" type="text">
-						</div>
-
-						<div class="form-group col-md-4">
-							<label>Ano/Serie</label>
-							<select name="ano_serie" class="form-control">
-								<option value="" disabled selected>Selecione</option>
-								<option value="0">1º Ano</option>
-								<option value="1">2º Ano</option>
-								<option value="2">3º Ano</option>
-								<option value="3">4º Ano</option>
-								<option value="4">5º Ano</option>
-								<option value="5">6º Ano</option>
-								<option value="6">7º Ano</option>
-								<option value="7">8º Ano</option>
-								<option value="8">9º Ano</option>
-								<option value="9">1º Ano/Médio</option>
-								<option value="10">2º Ano/Médio</option>
-								<option value="11">3º Ano/Médio</option>
-							</select>
-						</div>
-
+			<div class="panel panel-default">
+				<div class="panel-heading">Dados Acadêmicos</div>
+				<div class="panel-body">
+					<div class="col-md-12">
 						<div class="form-group col-md-6">
 							<label>Escola</label>
 							<input name="escola" disabled selected value="ESCOLA PROFESSOR CICERO MONTEIRO" class="form-control" type="text">
@@ -233,36 +204,28 @@
 
 						<div class="form-group col-md-6">
 							<label>Situação</label>
-							<div name="situacao" class="radio">
-								<label>
-									<input value="0" type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>  Novo
-								</label>
-
-								<label>
-									<input value="1" type="radio" name="optionsRadios" id="optionsRadios2" value="option2">  Transferido
-								</label>
-
-								<label>
-									<input value="2" type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Desistente
-								</label>
-
-								<label>
-									<input value="3" type="radio" name="optionsRadios" id="optionsRadios3" value="option3">  Repetente
-								</label>
-							</div>
+							<select name="situacao" class="form-control" required>
+								<option value="" disabled selected>Selecione</option>
+								<option value="0">Novo</option>
+								<option value="1">Transferido</option>
+								<option value="2">Desistente</option>
+								<option value="3">Repetente</option>
+							</select>
 						</div>
 
 						<div class="form-group col-md-12">
 							<label>Observação</label>
-							<textarea name="observacao_doiss" class="form-control" rows="5"></textarea>
+							<textarea name="obs" class="form-control" rows="5"></textarea>
 						</div>
-
-						<button type="submit" class="btn btn-primary">Confirmar</button>
-						<button type="reset" class="btn btn-default">Desfazer</button>
-					</form>
+					</div>
 				</div>
+			</div>
 
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<button type="submit" class="btn btn-primary"><b>Matricular Aluno</b></button>
+				</div>
 			</div>
 		</div>
-	</div>
+	</form>
 </div>
