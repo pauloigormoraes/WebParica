7cb455dd2b8a0f40d39edab00619bad2b88da7e1
paulomoraes <higoormoraes@gmail.com>
2016-05-26 03:43:16 -0400

    Minhas alteracoes final da noite

--- view/home.php
+++ view/home.php
@@ -5,71 +5,15 @@
 	</ol>
 </div>
 
-<div class="row">
-	<div class="col-lg-12">
-		<h1 class="page-header">Bem vindo ao PARICÁ</h1>
+<div class="row" style="text-align: center;">
+	<div class="col-lg-12" style="margin-top: 50px">
+		<img src='view/img/leafa.png'>
+		<h1 class="page-header" style="color: #91DC5A !important;">Bem vindo(a) ao Sistema Acadêmico - PARICÁ</h1>
 	</div>
 </div>
-
-<div class="row">
-	<div class="col-xs-12 col-md-6 col-lg-6">
-		<div class="panel panel-green panel-widget ">
-			<div class="row no-padding">
-				<div class="col-sm-3 col-lg-5 widget-left">
-					<svg class="glyph stroked bag"><use xlink:href="#stroked-plus-sign"></use></svg>
-				</div>
-				<a href="formulario.html">
-					<div class="col-sm-9 col-lg-7 widget-right">
-						<div class="large">Cadastrar</div>
-						<div class="text-muted">novo aluno</div>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-12 col-md-6 col-lg-6">
-		<div class="panel panel-green panel-widget ">
-			<div class="row no-padding">
-				<div class="col-sm-3 col-lg-5 widget-left">
-					<svg class="glyph stroked bag"><use xlink:href="#stroked-clipboard-with-paper"></use></svg>
-				</div>
-				<a href="buscar.html">
-					<div class="col-sm-9 col-lg-7 widget-right">
-						<div class="large">Buscar</div>
-						<div class="text-muted">aluno</div>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-12 col-md-6 col-lg-6">
-		<div class="panel panel-green panel-widget ">
-			<div class="row no-padding">
-				<div class="col-sm-3 col-lg-5 widget-left">
-					<svg class="glyph stroked bag"><use xlink:href="#stroked-blank-document"></use></svg>
-				</div>
-				<a href="#">
-					<div class="col-sm-9 col-lg-7 widget-right">
-						<div class="large">Ata de Reunião</div>
-						<div class="text-muted">professores</div>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-	<div class="col-xs-12 col-md-6 col-lg-6">
-		<div class="panel panel-green panel-widget ">
-			<div class="row no-padding">
-				<div class="col-sm-3 col-lg-5 widget-left">
-					<svg class="glyph stroked bag"><use xlink:href="#stroked-blank-document"></use></svg>
-				</div>
-				<a href="#">
-					<div class="col-sm-9 col-lg-7 widget-right">
-						<div class="large">Ata de Reunião</div>
-						<div class="text-muted">coordenação</div>
-					</div>
-				</a>
-			</div>
-		</div>
-	</div>
-</div>
+<!---->
+<!--<div class="row">-->
+<!--	<div class="index">-->
+<!--		<img src='view/img/castle.png'>-->
+<!--	</div>-->
+<!--</div>-->
