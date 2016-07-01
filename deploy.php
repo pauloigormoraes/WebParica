94bdc3a15021c29d49c1f7d9b41c87340a901b2b
Allex Lima <allexlima97@gmail.com>
2016-07-01 08:59:01 -0400

    Update

--- deploy.php
+++ deploy.php
@@ -1,10 +1,14 @@
 <?php
 
+	error_reporting(E_ALL | E_STRICT);
+	ini_set('display_errors', true);
+	header('Content-Type: text/html; charset=UTF-8');
+
 	if(@$_GET["pass"] == "bolinhodearroz")
 		print shell_exec("git pull origin master");
 	else{
 		header("HTTP/1.0 404 Not Found");
-		print "Error 404 e";
+		print "Error 404";
 	}
 
 ?>
