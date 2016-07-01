6f530dc992d747ad5658655d7e4cdbcd3c741642
paulomoraes <higoormoraes@gmail.com>
2016-07-01 19:26:49 -0400

    Finalizaçãoç parte 1

--- view/home.php
+++ view/home.php
@@ -1,25 +1,25 @@
 <div class="fundo">
-	<div class="row">
-		<ol class="breadcrumb">
-			<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
-			<li class="active">Início</li>
-		</ol>
-	</div>
-
-	<div class="fonte" style="text-align: center;">
-		<div class="col-lg-12" style="margin-top: 120px">
-			<img src='view/img/leafa.png'>
-			<h1 style="color: #91DC5A !important; text-shadow: 10px 10px 10px #99FF99;">
-				<b >Bem vindo(a) ao Sistema Acadêmico - PARICÁ</b></h1>
+		<div class="row">
+			<ol class="breadcrumb">
+				<li><a href="#"><svg class="glyph stroked home"><use xlink:href="#stroked-home"></use></svg></a></li>
+				<li class="active">Início</li>
+			</ol>
 		</div>
-	</div>
 
-	<div>
-		<div class="col-lg-12 font" style="margin-top: 130px">
-			<span style="font-size: 13px !important; text-align: center">
-				<p>Desenvolvido por <a style="color: #777 !important;" href="http://www.allexlima.com/" target="_blank">Allex Lima</a> e <a style="color: #777 !important;" href="http://www.pauloigormoraes.com/" target="_blank">Paulo Moraes</a> - UniNorte Laureate 2016 &copy; Todos os direitos reservados.</p>
-				<p>Projeto Idealizado por <a style="color: #777 !important;" href="http://www.pauloigormoraes.com/" target="_blank">Paulo Moraes</a> e Profº Ms.C Antônio Picanço</p>
-			</span>
+		<div class="row font" style="text-align: center;">
+			<div class="col-lg-12" style="margin-top: 120px">
+				<img src='view/img/leafa.png'>
+				<h1 style="color: #91DC5A !important; text-shadow: 10px 10px 10px #99FF99;">
+					<b>Bem vindo(a) ao Sistema Acadêmico - PARICÁ</b></h1>
+			</div>
 		</div>
-	</div>
+
+		<div>
+			<div class="col-lg-12 font" style="margin-top: 140px">
+				<span style="font-size: 13px !important; text-align: center">
+					<p>Desenvolvido por <a style="color: #777 !important;" href="http://www.allexlima.com/" target="_blank">Allex Lima</a> e <a style="color: #777 !important;" href="http://www.moraespaulo.com/" target="_blank">Paulo Moraes</a> - UniNorte Laureate 2016 &copy; Todos os direitos reservados.</p>
+					<p>Projeto Idealizado por <a style="color: #777 !important;" href="http://www.moraespaulo.com/" target="_blank">Paulo Moraes</a> e Profº Ms.C Antônio Picanço</p>
+				</span>
+			</div>
+		</div>
 </div>
